9e3aacdfd19bc3eaebee5c84f832534f3290f5b1
John <john_r92@hotmail.co.uk>
2025-02-25 19:33:43 +0000

    Added styling for tracker app

--- components/SnookerTracker.tsx
+++ components/SnookerTracker.tsx
@@ -1,3 +1,48 @@
+import React from 'react'
+import { LuSave } from 'react-icons/lu'
+import { RxReset } from 'react-icons/rx'
+
 export default function SnookerTracker() {
-  return 0
+  return (
+    <div>
+      <div className="flex justify-center items-center pt-14">
+        <div className="ms-24 me-24">
+          <button className="bg-rose-700 hover:bg-rose-500 shadow-gray-800 shadow-md p-3 rounded-md text-white">
+            <RxReset className="w-8 h-8" />
+          </button>
+        </div>
+        <div className="ms-24 me-24">
+          <button className="bg-green-700 hover:bg-green-500 shadow-gray-800 shadow-md p-3 rounded-md text-white">
+            <LuSave className="w-8 h-8" />
+          </button>
+        </div>
+      </div>
+
+      <div className="m-80"></div>
+
+      <div className="flex justify-center items-center pt-14">
+        <button className="bg-rose-700 hover:bg-rose-500 shadow-gray-800 shadow-md m-2 p-3 rounded-md text-white">
+          <RxReset className="w-8 h-8" />
+        </button>
+        <button className="bg-green-700 hover:bg-green-500 shadow-gray-800 shadow-md m-2 p-3 rounded-md text-white">
+          <LuSave className="w-8 h-8" />
+        </button>
+        <button className="bg-rose-700 hover:bg-rose-500 shadow-gray-800 shadow-md m-2 p-3 rounded-md text-white">
+          <RxReset className="w-8 h-8" />
+        </button>
+        <button className="bg-green-700 hover:bg-green-500 shadow-gray-800 shadow-md m-2 p-3 rounded-md text-white">
+          <LuSave className="w-8 h-8" />
+        </button>
+        <button className="bg-rose-700 hover:bg-rose-500 shadow-gray-800 shadow-md m-2 p-3 rounded-md text-white">
+          <RxReset className="w-8 h-8" />
+        </button>
+        <button className="bg-green-700 hover:bg-green-500 shadow-gray-800 shadow-md m-2 p-3 rounded-md text-white">
+          <LuSave className="w-8 h-8" />
+        </button>
+        <button className="bg-green-700 hover:bg-green-500 shadow-gray-800 shadow-md m-2 p-3 rounded-md text-white">
+          <LuSave className="w-8 h-8" />
+        </button>
+      </div>
+    </div>
+  )
 }
